58e251139aa63792d7f230f58f8ff9fea8a74b3c
kinjal <kkanjiya849@rku.ac.in>
2023-08-08 13:51:48 +0530

    Mongo_connect

--- client/backend/db.js
+++ client/backend/db.js
@@ -1,1 +1,13 @@
 const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://GoFood:GoFood_1234@cluster0.kojmg9f.mongodb.net/?retryWrites=true&w=majority';
+
+const mongoDB = async () => {
+    try {
+        await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+};
+
+module.exports = mongoDB;
--- client/backend/index.js
+++ client/backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
  const port = 3000
-
+const mongoDB = require("./db")
+mongoDB();
  app.get('/',(req,res)=>{
     res.send("Hello World")
  })
